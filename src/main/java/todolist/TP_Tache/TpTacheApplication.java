80027c8ee1a790e2db13f7752031123c89f3504e
OUMANE THIAM <ousmanemoussathiam@gmail.com>
2021-05-04 10:24:14 +0200

    add comment

--- src/main/java/todolist/TP_Tache/TpTacheApplication.java
+++ src/main/java/todolist/TP_Tache/TpTacheApplication.java
@@ -1,6 +1,6 @@
 package todolist.TP_Tache;
 
-import java.text.SimpleDateFormat;
+
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +8,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import todolist.TP_Tache.dao.TacheRepository;
+
 import todolist.TP_Tache.metier.Tache;
+
 
 @SpringBootApplication
 @ComponentScan(basePackages ="todolist.TP_Tache.metier,todolist.TP_Tache.controller")
@@ -20,6 +23,7 @@ import todolist.TP_Tache.metier.Tache;
 public class TpTacheApplication implements CommandLineRunner {
 	@Autowired
 	private TacheRepository tacheRepository;
+	
 	@Autowired
 	private RepositoryRestConfiguration restConfiguration;
 
@@ -31,9 +35,11 @@ public class TpTacheApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		restConfiguration.exposeIdsFor(Tache.class);
-		tacheRepository.save(new Tache(null,"maths","etudes", new Date()));
-		tacheRepository.save(new Tache(null,"maths","etudes", new Date()));
-		tacheRepository.save(new Tache(null,"maths","etudes", new Date()));
+		tacheRepository.save(new Tache(null,"maths","etudes",0, new Date(),new Date()));
+		tacheRepository.save(new Tache(null,"maths","etudes",0, new Date(),new Date()));
+		tacheRepository.save(new Tache(null,"maths","etudes",0,new Date(),new Date()));
+		
+		
 		
 		tacheRepository.findAll().forEach(p->{
 			System.out.println(p.toString());
